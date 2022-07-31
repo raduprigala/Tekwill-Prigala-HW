a539228569ac2b6a350c554e6cf0a2adf17100c3
Irina Sevcenco <prigala.r@gmail.com>
2022-07-31 15:13:57 +0300

    tema pe acasa

---
+++ Lesson_6_hw.java
@@ -0,0 +1,16 @@
+package HomeWorkTekwill;
+
+public class Lesson_6_hw {
+    public static void main(String[] args) {
+        int x = 13;
+        if ((x%2==2)&&(x>10)){
+            System.out.println("numarul este par");
+        } else if ((x%2!=0)&&(x<15)){
+            System.out.println("numarul este impar");
+
+        }
+
+
+    }
+}
+
